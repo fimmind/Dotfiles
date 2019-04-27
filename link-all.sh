cceda691ca8404d013bd83d2721f46dd148de3af
fimmind <grayfox19@mail.ru>
2019-04-27 12:21:57 +0300

    gtk theme

--- link-all.sh
+++ link-all.sh
@@ -4,9 +4,13 @@ curDir=$(pwd)
 if [ ! -d ~/.config ]; then
   mkdir ~/.config
 fi
+if [ ! -d ~/.config/gtk-3.0 ]; then
+  mkdir ~/.config/gtk-3.0
+fi
 
-ln -si $curDir/i3          ~/.config/
-ln -si $curDir/.vim        ~/
-ln -si $curDir/.Xresources ~/
-ln -si $curDir/.zshrc      ~/
-ln -si $curDir/.keynavrc   ~/
+ln -s $1 $curDir/i3                   ~/.config/
+ln -s $1 $curDir/gtk-3.0/settings.ini ~/.config/gtk-3.0/
+ln -s $1 $curDir/.vim                 ~/
+ln -s $1 $curDir/.Xresources          ~/
+ln -s $1 $curDir/.zshrc               ~/
+ln -s $1 $curDir/.keynavrc            ~/
