9ede0ee5933d80e197c7a71878df991d6086b205
fimmind <grayfox19@mail.ru>
2019-05-02 19:03:33 +0300

    Delete gtk settings. Add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# My dotfiles
+## GTK theme
+Use `lxappearance`.
+
+Favorite themes:
+* Widget: Plata-Noir
+* Icon: Papirus
+* Mouse cursor: Breeze
+
+## TODO
+* Apps and dependencies installation
--- link-all.sh
+++ link-all.sh
@@ -4,13 +4,9 @@ curDir=$(pwd)
 if [ ! -d ~/.config ]; then
   mkdir ~/.config
 fi
-if [ ! -d ~/.config/gtk-3.0 ]; then
-  mkdir ~/.config/gtk-3.0
-fi
 
 ln -s $1 $curDir/i3                   ~/.config/
 ln -s $1 $curDir/i3status             ~/.config/
-ln -s $1 $curDir/gtk-3.0/settings.ini ~/.config/gtk-3.0/
 ln -s $1 $curDir/.vim                 ~/
 ln -s $1 $curDir/.Xresources          ~/
 ln -s $1 $curDir/.zshrc               ~/
