49e3a998b378f5b4fb69f12e8194d1bda4ebea8b
fimmind <grayfox19@mail.ru>
2019-05-05 15:58:46 +0300

    i3: better parting. Disable custom workspace names. Add Screenshot hotkeys

--- i3/MoveToWorkspace.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-from os import *
-
-name = popen('./inputWorkspaceName.py').read()
-system('i3-msg move container to workspace "' + name + '"')
-system('i3-msg workspace "' + name + '"')
