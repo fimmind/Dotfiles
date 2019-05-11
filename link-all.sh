a4e08413b3ca01345f1ca52297037c9f55c772a0
fimmind <grayfox19@mail.ru>
2019-05-11 21:44:51 +0300

    Use manjaro's .Xresources file (Only added rofi settings) Do not automaticly replace .Xresource if it already exist Support chromium for deezer app Keynav autostart i3 Desktop backgound

--- link-all.sh
+++ link-all.sh
@@ -5,10 +5,12 @@ if [ ! -d ~/.config ]; then
   mkdir ~/.config
 fi
 
-ln -s $1 $curDir/i3                   ~/.config/
-ln -s $1 $curDir/i3status             ~/.config/
-ln -s $1 $curDir/.vim                 ~/
-ln -s $1 $curDir/.Xresources          ~/
-ln -s $1 $curDir/.zshrc               ~/
-ln -s $1 $curDir/.keynavrc            ~/
-ln -s $1 $curDir/deezer.desktop       ~/.local/share/applications/
+ln -s $1 $curDir/i3                      ~/.config/
+ln -s $1 $curDir/i3status                ~/.config/
+ln -s $1 $curDir/.vim                    ~/
+ln -s $1 $curDir/.zshrc                  ~/
+ln -s $1 $curDir/.keynavrc               ~/
+ln -s $1 $curDir/deezer/deezer.desktop   ~/.local/share/applications/
+ln -s $1 $curDir/.fehbg                  ~/
+
+sudo ln -s $1 $curDir/deezer/deezer       /usr/bin/
