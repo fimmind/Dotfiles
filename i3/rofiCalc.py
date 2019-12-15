662964c7d7cd51841a93deb14cbcecd7e283a9e4
fimmind <fimmind@mail.ru>
2019-12-15 16:36:30 +0300

    Format rofiCalc.py

--- i3/rofiCalc.py
+++ i3/rofiCalc.py
@@ -6,5 +6,5 @@ history = ""
 
 equation = popen(rofiCmd).read()
 while equation and equation != "\n" and equation != "exit\n":
-  history = popen(f"qalc '{equation}'").read() + history
-  equation = popen(f"{rofiCmd} -mesg '{history[0:-1]}'").read()
+    history = popen(f"qalc '{equation}'").read() + history
+    equation = popen(f"{rofiCmd} -mesg '{history[0:-1]}'").read()
