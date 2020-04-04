02edbaeb5f865c6245a7f678e702037aa7eaa455
fimmind <fimmind@mail.ru>
2020-04-04 16:44:52 +0300

    Clojure: setup hashp
    https://github.com/weavejester/hashp

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.22.4"]]
-        :dependencies [[alembic "0.3.2"]]}}
+        :dependencies [[alembic "0.3.2"]
+                       [hashp "0.1.1"]]
+        :injections [(require 'hashp.core)]}}
