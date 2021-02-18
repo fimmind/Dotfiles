93164b3e5041f646548c2c56db04d0b4c34aa19c
fimmind <fimmind@mail.ru>
2021-02-18 14:40:39 +0300

    setup.sh: always cleanup on exit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #! /usr/bin/env bash
 
 sudoers=$(sudo mktemp /etc/sudoers.d/dotmake_${USER}_XXXXXX) || exit
+cleanup_exit() {
+    sudo rm $sudoers &> /dev/null
+    exit
+}
+trap cleanup_exit \
+    SIGALRM SIGHUP SIGINT SIGIO \
+    SIGKILL SIGPIPE SIGPROF SIGPWR \
+    SIGSTKFLT SIGTERM EXIT ERR
+
 echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee $sudoers
 
-sudo pacman-key --refresh-keys
+sudo pacman-key --refresh-keys || true
 sudo pacman -Syu --noconfirm
 
 dotmake install base_pkgs
@@ -13,4 +22,4 @@ else
     dotmake install $@
 fi
 
-sudo rm $sudoers
+cleanup_exit
