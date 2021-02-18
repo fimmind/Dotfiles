76d42b3ec00f5fb9f374eb41b33f4ccd5ff0fb5c
fimmind <fimmind@mail.ru>
2021-02-18 13:40:25 +0300

    Setup.sh: always run pacman's updates

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 sudoers=$(sudo mktemp /etc/sudoers.d/dotmake_${USER}_XXXXXX) || exit
 echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee $sudoers
 
+sudo pacman-key --refresh-keys
+sudo pacman -Syu --noconfirm
+
+dotmake install base_pkgs
 if [ $# -eq 0 ]; then
-    sudo pacman-key --refresh-keys
-    sudo pacman -Suy --noconfirm
-    dotmake install base_pkgs && sudo ldconfig && \
-        dotmake install DE pr_langs
+    dotmake install DE pr_langs texlive
 else
-    dotmake install base_pkgs && \
-        dotmake install $@
+    dotmake install $@
 fi
 
 sudo rm $sudoers
